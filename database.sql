38ddf6c5a3144815229bc108b0b881747ffba480
Emir Cengiz <emircengiz21@gmail.com>
2022-07-27 20:29:16 +0300

    Fix typos

--- database.sql
+++ database.sql
@@ -1,77 +1,76 @@
-
 DROP TABLE IF EXISTS userinfo;
-
+--DEFAULT(CURRENT_DATE)
 CREATE TABLE IF NOT EXISTS userinfo (
-    userid biginit insigned NOT NULL,
+    userid BIGINT UNSIGNED NOT NULL,
     username TEXT DEFAULT "",
     country INTEGER DEFAULT 0,
     banned INTEGER DEFAULT 0,
     bannedreason TEXT DEFAULT "",
     privilages INTEGER DEFAULT 0,
-    joinedat DATE DEFAULT (CURRENT_DATE),
-);
+    joinedat DATE DEFAULT NULL
+);   
 
 DROP TABLE IF EXISTS authenticationpairs;
 
 CREATE TABLE IF NOT EXISTS authenticationpairs (
-    userid biginit unsigned NOT NULL,
-    password TEXT DEFAULT "",
+    userid BIGINT UNSIGNED NOT NULL,
+    password TEXT DEFAULT ""
 );
 
 DROP TABLE IF EXISTS friends;
 
 CREATE TABLE IF NOT EXISTS friends (
-    userid biginit unsigned NOT NULL,
-    friendid biginit unsigned NOT NULL,
+    userid BIGINT UNSIGNED NOT NULL,
+    friendid BIGINT UNSIGNED NOT NULL
 );
 
 DROP TABLE IF EXISTS userpage;
 
 CREATE TABLE IF NOT EXISTS userpage (
-    userid bigint unsigned NOT NULL,
+    userid BIGINT UNSIGNED NOT NULL,
     title TEXT DEFAULT "",
     website TEXT DEFAULT "",
     aboutme TEXT DEFAULT "",
-    twitter TEXT DEFAULT "",
+    twitter TEXT DEFAULT ""
 );
 
 DROP TABLE IF EXISTS inventory;
 
 CREATE TABLE IF NOT EXISTS inventory (
-    userid bigint unsigned NOT NULL,
-    itemid bigint unsigned NOT NULL,
-    itemcount bigint unsigned DEFAULT 1,
+    userid BIGINT UNSIGNED NOT NULL,
+    itemid BIGINT UNSIGNED NOT NULL,
+    itemcount BIGINT UNSIGNED DEFAULT 1
 );
 
 DROP TABLE IF EXISTS worlds;
 
 CREATE TABLE IF NOT EXISTS worlds (
-    worldid bigint unsigned NOT NULL,
-    mapid biginit unsigned NOT NULL,
+    worldid BIGINT UNSIGNED NOT NULL,
+    mapid BIGINT UNSIGNED NOT NULL
 );
 
-DROP TABLE IF NOT EXISTS usersession;
+DROP TABLE IF EXISTS usersession;
 
 CREATE TABLE IF NOT EXISTS usersession (
-    userid bigint unsigned NOT NULL,
-    worldid bigint unsigned NOT NULL,
-    prevworldid bigint unsigned NOT NULL,
+    userid BIGINT UNSIGNED NOT NULL,
+    worldid BIGINT UNSIGNED NOT NULL,
+    prevworldid BIGINT UNSIGNED NOT NULL,
 
     position_x bigint DEFAULT 0,
     position_y bigint DEFAULT 0,
 
-    headitem bigint unsigned DEFAULT 0,
-    torsoitem bigint unsigned DEFAULT 0,
-    legsitem bigint unsigned DEFAULT 0,
-    shoesitem bigint unsigned DEFAULT 0,
+    headitem BIGINT UNSIGNED DEFAULT 0,
+    torsoitem BIGINT UNSIGNED DEFAULT 0,
+    legsitem BIGINT UNSIGNED DEFAULT 0,
+    shoesitem BIGINT UNSIGNED DEFAULT 0,
 
-    headitemsecondary bigint unsigned DEFAULT 0,
-    torsoitemsecondary bigint unsigned DEFAULT 0,
-    legsitemsecondary bigint unsigned DEFAULT 0,
-    shoesitemsecondary bigint unsigned DEFAULT 0,
+    headitemsecondary BIGINT UNSIGNED DEFAULT 0,
+    torsoitemsecondary BIGINT UNSIGNED DEFAULT 0,
+    legsitemsecondary BIGINT UNSIGNED DEFAULT 0,
+    shoesitemsecondary BIGINT UNSIGNED DEFAULT 0,
 
-    primaryweapon bigint unsigned DEFAULT 0,
-    secondaryweapon bigint unsigned DEFAULT 0,
-    pet bigint unsigned DEFAULT 0,
+    primaryweapon BIGINT UNSIGNED DEFAULT 0,
+    secondaryweapon BIGINT UNSIGNED DEFAULT 0,
+    pet BIGINT UNSIGNED DEFAULT 0
 );
 
